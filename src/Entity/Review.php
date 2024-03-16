06429a8fa71776750dd8f7abc0d468eb2c83c2b2
MattDaemon1 <matt@mattkonnect.com>
2024-03-16 21:46:41 +0100

    Ajout des entités et relation user_review

---
+++ src/Entity/Review.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReviewRepository;
+use Doctrine\ORM\Mapping as ORM;
+use PHPUnit\TextUI\XmlConfiguration\CodeCoverage\Report\Text;
+
+#[ORM\Entity(repositoryClass: ReviewRepository::class)]
+class Review
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?smallint $rate = null;
+
+    #[ORM\Column(length: 255)]
+    private ?Text $review = null;
+
+    #[ORM\Column]
+    private ?bool $approved = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reviews')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?user $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRate(): ?string
+    {
+        return $this->rate;
+    }
+
+    public function setRate(string $rate): static
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+
+    public function getReview(): ?string
+    {
+        return $this->review;
+    }
+
+    public function setReview(string $review): static
+    {
+        $this->review = $review;
+
+        return $this;
+    }
+
+    public function isApproved(): ?bool
+    {
+        return $this->approved;
+    }
+
+    public function setApproved(bool $approved): static
+    {
+        $this->approved = $approved;
+
+        return $this;
+    }
+
+    public function getUser(): ?user
+    {
+        return $this->user;
+    }
+
+    public function setUser(?user $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
